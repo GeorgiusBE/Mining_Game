2164fce10a1a0f45306e8ed32da2ca16c33a125a
Georgius <georgiusbenedikt09@gmail.com>
2024-11-14 16:00:40 +0000

    Update on BlockChain initialization
    Added machine status (on/off) into input of dictionary of lists. This is to ensure that users with machines that are turned off are not considered as a player.

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,22 @@
 class BlockChain:
     def __init__(self, user_config):
         '''
-        user_config -> (Dict of lists) user_name(str) : [mining_type(str of either 'solo' or 'pooled'), number of machines]
+        user_config -> (Dict of lists) user_name(str) : [machine status ('on'/'off'), mining_type(str of either 'solo' or 'pooled'), number of machines]
         '''
         self.user_config = user_config
 
-        # create a distionary to store the mining players
+        # create a dictionary to store the (mining) players
         self.mining_players = {'pooled':1000}
-        for user,config in user_config:
+        for user_name,config in user_config:
+            # users with machines switched off
+            if config[0] == 'off':
+                # no participation
+                self.mining_players[user_name] = 0
             # users with solo mining type
-            if config[0] == 'solo':
-                self.mining_players[user] = config[1]
+            elif config[1] == 'solo':
+                # participate as an individual player
+                self.mining_players[user_name] = config[2]
             # users with pooled mining type
-            elif config[0] == 'pooled':
-                self.mining_players['pooled'] += config[1]
+            elif config[1] == 'pooled':
+                # participate as part of a group
+                self.mining_players['pooled'] += config[2]
