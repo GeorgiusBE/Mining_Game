b639fa4e1dcb1702a163bc4dd8e9992a5251c3c5
Georgius <georgiusbenedikt09@gmail.com>
2024-11-16 03:19:50 +0000

    Remove unncessary in the user prompt when swithing mining type
    Switching the mining type, now only takes 1 step (i.e. select action 4), whereas before it took 2 steps (i.e. select action 4 and then manually type solo or pooled).

--- user_account.py
+++ user_account.py
@@ -32,27 +32,6 @@ class UserAccount:
         # update capital
         self.capital -= 600 * n_machines
 
-    # # switch the machines on/off
-    # def machine_swith(self, switch):
-    #     '''
-    #     Not to be used alone
-    #     switch -> it accepts either 'on' or 'off' (str)
-    #     '''
-    #     try:
-    #         # turn on the machines
-    #         if switch == 'on':
-    #             self.machine_status = 'on'
-    #         # turn off the machines
-    #         elif switch == 'off':
-    #             self.machine_status = 'off'
-    #         # raise error if any other values are entered
-    #         else:
-    #             raise ValueError("Invalid value: It only accepts 'on' or 'off'")
-
-    #     # print error message    
-    #     except ValueError as err:
-    #         print(err)
-
     # switch the machines on/off
     def machine_swith(self):
         '''
@@ -86,27 +65,17 @@ class UserAccount:
         except ValueError as err:
             print(err)
 
-    
     # switch mining type (solo/pooled)
-    def change_mining_type(self, mine_type):
+    def change_mining_type(self):
         '''
         Not to be used alone
-        mine_type -> (str) 'solo' or 'pooled'
         '''
-        try:
-            # change mining type to solo
-            if mine_type == 'solo':
-                self.mining_type = 'solo'
-            # change mining type to pooled
-            elif mine_type == 'pooled':
-                self.mining_type = 'pooled'
-            # raise error if any other values are entered
-            else:
-                raise ValueError("Invalid value: It only accepts 'solo' or 'pooled'")
-
-        # print error message    
-        except ValueError as err:
-            print(err)
+        # change mining type to pooled if current mining type is solo
+        if self.mining_type == 'solo':
+            self.mining_type = 'pooled'
+        # change mining type to solo if current mining type is pooled
+        elif self.mining_type == 'pooled':
+            self.mining_type = 'solo'
 
     # query user to pick an action
     def action_query(self, action, sdpa_price):
@@ -163,10 +132,8 @@ class UserAccount:
                 if self.machines == 0:
                     raise ValueError("No machines owned: Cannot switch mining type without owning any machines.")
 
-                # query for mining type
-                mine_type = input("Enter mining type ['solo'/'pooled']: ")
                 # update mining type
-                self.change_mining_type(mine_type)
+                self.change_mining_type()
 
             # print error message
             except ValueError as err:
